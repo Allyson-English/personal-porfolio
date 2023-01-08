046ad21c5e4b70871f7738fdae22e8acf45185ec
Allyson-English <allyson.english@gmail.com>
2023-01-08 16:26:39 -0700

    dimo mobile above the fold

--- src/layouts/Dimo/index.tsx
+++ src/layouts/Dimo/index.tsx
@@ -24,6 +24,56 @@ export const Dimo = () => {
           </div>
         </div>
 
+        <p className="text-white font-normal text-[232px] text-center">
+          DIMO Mobile
+        </p>
+
+        <div className="container mx-auto flex justify-between border-b-[1px] pb-20">
+          <table className="text-xl">
+            <tbody>
+              <tr>
+                <td className="text-gray-400">Client</td>
+                <td className="text-white text-right">DIMO</td>
+              </tr>
+              <tr>
+                <td className="text-gray-400">Type</td>
+                <td className="text-white text-right">Mobile App, Brand</td>
+              </tr>
+              <tr>
+                <td className="text-gray-400">Role</td>
+                <td className="text-white text-right">Lead Designer</td>
+              </tr>
+              <tr>
+                <td className="text-gray-400">Team</td>
+                <td className="text-white text-right pl-10">
+                  Dora Tang (Design), Abi Paine (3D)
+                </td>
+              </tr>
+              <tr>
+                <td className="text-gray-400">Year</td>
+                <td className="text-white text-right">2021-2022</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div className="max-w-xl">
+            <div className="float-left h-6 w-20 flex items-center">
+              <span className="text-gray-400 text-sm">About</span>
+            </div>
+            <p className="text-xl text-white leading-snug">
+              DIMO is an open-source web3 project which enables users to collect
+              and monetize the data from their vehicles in exchange for a
+              governance token. The core of the project is its mobile app.
+            </p>
+          </div>
+
+          <div className="">
+            <div className="h-40 w-40 flex items-center justify-center border border-white rounded-full">
+              <div className="text-xs text-white">Live Project</div>
+            </div>
+          </div>
+        </div>
+
         {/* FOOTER */}
         <div className="h-16 w-full bg-transparent fixed bottom-0">
           <div className="container mx-auto flex justify-between items-center h-full text-white text-sm">
