dfb915970a8c4a2e1e967e0028ebb6a0527ac850
Allyson-English <allyson.english@gmail.com>
2023-02-06 21:51:33 -0500

    nav

--- src/layouts/Pods/index.tsx
+++ src/layouts/Pods/index.tsx
@@ -63,7 +63,9 @@ export const Pods = () => {
         <BoldStatement text="Lorem ipsum get the file on Figma Community. Lorem ipsum number of copies made." />
         <BottomNavigation
           previousProject="DIMO Mobile"
+          previousProjectURL="./#/projects/daniel-ricciardo"
           nextProject="Mint Sunday"
+          nextProjectURL="../#/projects/daniel-ricciardo"
         />
 
         <CallToAction />
